61745ce0fc95346d313e42c039978f309d56bb2f
reda.ezzriouli <reda.ezziouli@davidson.fr>
2024-04-15 20:43:06 +0200

    Unit tests : all the major problems that we could face while parsing

--- src/main/scala/services/Parser.scala
+++ src/main/scala/services/Parser.scala
@@ -2,22 +2,51 @@ package services
 
 import models.{Orientation, Pelouse, Position, Tondeuse}
 import scala.io.Source
+import scala.util.{Try, Failure, Success}
 
 object Parser {
   def readInstructions(filePath: String): (Pelouse, List[(Tondeuse, String)]) = {
-    val lines = Source.fromFile(filePath).getLines().toList
-    val dimensions = lines.head.split(" ").map(_.toInt)
-    val pelouse = Pelouse(dimensions(0), dimensions(1))
+    val source = Source.fromFile(filePath)
+    Try {
+      try {
+        val lines = source.getLines().toList
+        if (lines.isEmpty) {
+          throw new IllegalArgumentException("Le fichier est vide")
+        }
 
-    val tondeuses = lines.tail.grouped(2).map {
-      case List(line1, line2) =>
-        val posData = line1.split(" ")
-        val position = Position(posData(0).toInt, posData(1).toInt)
-        val orientation = Orientation.fromChar(posData(2).head).getOrElse(throw new IllegalArgumentException("Orientation invalide"))
-        val tondeuse = Tondeuse(position, orientation)
-        (tondeuse, line2)
-    }.toList
+        val dimensions = lines.headOption.flatMap { line =>
+          val parts = line.split(" ").map(_.trim).filter(_.nonEmpty).map(_.toInt)
+          if (parts.length == 2) Some(parts) else None
+        }.getOrElse(throw new IllegalArgumentException("Dimensions manquantes ou incorrectes pour la pelouse"))
 
-    (pelouse, tondeuses)
+        val pelouse = Pelouse(dimensions(0), dimensions(1))
+
+        val tondeuses = lines.tail.grouped(2).toList.flatMap {
+          case List(line1, line2) =>
+            val posData = line1.split(" ").map(_.trim).filter(_.nonEmpty)
+            if (posData.length != 3) throw new IllegalArgumentException("Données de tondeuse invalides ou incomplètes")
+
+            val position = Position(posData(0).toInt, posData(1).toInt)
+            val orientation = Orientation.fromChar(posData(2).head).getOrElse(throw new IllegalArgumentException("Orientation invalide"))
+
+            if (!line2.matches("[AGD]+")) {
+              throw new IllegalArgumentException("Instructions contiennent des caractères invalides")
+            }
+
+            Some(Tondeuse(position, orientation), line2)
+          case List(line1, _) =>
+            throw new IllegalArgumentException("Instructions manquantes pour une tondeuse")
+          case _ =>
+            throw new IllegalArgumentException("Données de tondeuse incomplètes")
+        }
+
+        (pelouse, tondeuses)
+      } finally {
+        source.close()
+      }
+    } match {
+      case Success(value) => value
+      case Failure(ex) => throw ex
+    }
   }
 }
--- src/test/scala/ParserTest.scala
+++ src/test/scala/ParserTest.scala
@@ -1,10 +1,10 @@
 import models.{Orientation, Pelouse, Position, Tondeuse}
+import services.Parser
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
-import services.Parser
+import java.io.FileNotFoundException
 
-
-class ParserTest extends AnyFlatSpec with Matchers  {
+class ParserTest extends AnyFlatSpec with Matchers {
 
   "readInstructions" should "correctly parse the input file into models" in {
     val (pelouse, tondeuses) = Parser.readInstructions("src/test/resources/test_instructions.txt")
@@ -12,5 +12,47 @@ class ParserTest extends AnyFlatSpec with Matchers  {
     tondeuses.length should be(2)
     tondeuses.head._1 should be(Tondeuse(Position(1, 2), Orientation.North))
     tondeuses.head._2 should be("GAGAGAGAA")
+  }
+
+  it should "throw an IllegalArgumentException if the file is empty" in {
+    intercept[IllegalArgumentException] {
+      Parser.readInstructions("src/test/resources/emptyy.txt")
+    }
+  }
+
+  it should "throw an IllegalArgumentException for incorrect 'Pelouse' dimensions" in {
+    intercept[IllegalArgumentException] {
+      Parser.readInstructions("src/test/resources/missing_first.txt")
+    }
+  }
+
+  it should "throw an IllegalArgumentException for invalid 'Tondeuse' data" in {
+    intercept[IllegalArgumentException] {
+      Parser.readInstructions("src/test/resources/missing_second.txt")
+    }
+  }
+
+  it should "throw an IllegalArgumentException for invalid orientation" in {
+    intercept[IllegalArgumentException] {
+      Parser.readInstructions("src/test/resources/invalid_orientation.txt")
+    }
+  }
+
+  it should "handle files with missing 'Tondeuse' instructions" in {
+    intercept[IllegalArgumentException] {
+      Parser.readInstructions("src/test/resources/missing_mower.txt")
+    }
+  }
+
+  it should "detect invalid characters in instructions" in {
+    intercept[IllegalArgumentException] {
+      Parser.readInstructions("src/test/resources/invalid_instruction_chars.txt")
+    }
+  }
+
+  it should "throw a RuntimeException for non-existent file paths" in {
+    intercept[FileNotFoundException] {
+      Parser.readInstructions("src/test/resources/inexistent.txt")
+    }
   }
 }
