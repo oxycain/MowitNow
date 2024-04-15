f5b806ed8802a8f03ddf61b81ae0c967499021e2
reda.ezzriouli <reda.ezziouli@davidson.fr>
2024-04-15 16:56:26 +0200

    first version of a GUI : compiling and working

--- build.sbt
+++ build.sbt
@@ -2,7 +2,10 @@ ThisBuild / version := "0.1.0-SNAPSHOT"
 
 ThisBuild / scalaVersion := "2.11.12"
 libraryDependencies += "org.scalatest" %% "scalatest" % "3.2.17" % Test
-
+libraryDependencies ++= Seq(
+	"org.scala-lang.modules" %% "scala-swing" % "3.0.0",
+	"org.scalatest" %% "scalatest" % "3.2.17" % Test
+)
 lazy val root = (project in file("."))
   .settings(
     name := "mowitnow"
---
+++ src/main/scala/gui/TondeuseGUI.scala
@@ -0,0 +1,63 @@
+package gui
+
+import scala.swing._
+import scala.swing.event._
+import javax.swing.ImageIcon
+import services.Simulation
+import java.io.File
+import java.awt.{Graphics2D, Image}
+
+class ImagePanel(imagePath: String) extends Panel {
+  private val image: Image = new ImageIcon(imagePath).getImage
+
+  override def paintComponent(g: Graphics2D): Unit = {
+  super.paintComponent(g)
+  if (image != null) {
+    g.drawImage(image, 0, 0, peer.getWidth, peer.getHeight, peer)
+  }
+  }
+
+}
+
+object TondeuseGUI extends SimpleSwingApplication {
+  def top = new MainFrame {
+    title = "Simulateur de Tondeuse"
+    preferredSize = new Dimension(800, 600)
+
+
+    val pelousePanel = new ImagePanel("resources/mow-your-lawn.jpg") {
+      preferredSize = new Dimension(600, 300)
+      border = Swing.EmptyBorder(10, 10, 10, 10)
+    }
+
+    val button = new Button("Jardiner") {
+      preferredSize = new Dimension(80, 40)
+    }
+
+    val fileChooser = new FileChooser(new File("."))
+    fileChooser.title = "Choisir un fichier d'instructions"
+
+    contents = new BoxPanel(Orientation.Vertical) {
+      contents += pelousePanel
+      contents += Swing.VStrut(10)
+      contents += new BoxPanel(Orientation.Horizontal) {
+        contents += button
+      }
+      border = Swing.EmptyBorder(10, 10, 10, 10)
+    }
+
+    listenTo(button)
+
+    reactions += {
+      case ButtonClicked(`button`) =>
+        val result = fileChooser.showOpenDialog(null)
+        if (result == FileChooser.Result.Approve) {
+          val (pelouse, tondeuses) = Simulation.readInstructions(fileChooser.selectedFile.getAbsolutePath)
+          tondeuses.zipWithIndex.foreach { case ((tondeuse, instructions), index) =>
+            val result = Simulation.executeInstructions(pelouse, tondeuse, instructions)
+            Dialog.showMessage(contents.head, s"La tondeuse $index se trouve à (${result.position.x}, ${result.position.y}) orientée ${result.orientation}", title="Position Finale")
+          }
+        }
+    }
+  }
+}
